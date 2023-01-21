03f911ca6d4247c8e1678a6a7341f196e38d7045
AMess33 <amess3309@gmail.com>
2023-01-21 13:26:50 -0600

    add pseudo code to js

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
 $(function () {
-
+// get date from dayjs x
+// put todays date into html doc x
    var today = dayjs();
    $('#todaysDate').text(today.format('dddd, MMMM D'))
+
+//    take input from search bar
+// run search through weather api
+// retreive 5 day forcast from api
+// append 5 day forcast to html page
 
 
 

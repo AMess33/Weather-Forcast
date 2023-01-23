013ae12f4d36c6c388495a789a2994a7cd6ef397
AMess33 <amess3309@gmail.com>
2023-01-22 21:42:23 -0600

    add event listnere for city search

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,22 @@
 $(function () {
-// get date from dayjs x
-// put todays date into html doc x
+// // get date from dayjs x
+// // put todays date into html doc x
    var today = dayjs();
    $('#todaysDate').text(today.format('dddd, MMMM D'))
-
-//    take input from search bar
-// run search through weather api
-// retreive 5 day forcast from api
-// append 5 day forcast to html page
-
-
-
-    
-    
 });
+//  event listener for form submit X
+// create api call from form submit city info
+// call api to get longitude and latitude of city
+
+// add event listener X
+   // get value from input and place in variable X
+      // add searched city to h2 element on html page X
+var searchEl = document.getElementById("searchBtn");
+var currentCity = document.getElementById("city");
+
+searchEl.addEventListener('click', Event => {
+   var searchCityVal = document.querySelector('#searchCity').value;
+   console.log(searchCityVal);
+   currentCity.textContent = searchCityVal;
+});
+    

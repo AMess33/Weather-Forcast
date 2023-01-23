2a574bb71a0d311bccb734ac4cc1113f0295a0f1
AMess33 <amess3309@gmail.com>
2023-01-22 22:30:11 -0600

    get lon and lat from api call create new url for forcast api call

--- assets/script.js
+++ assets/script.js
@@ -11,12 +11,34 @@ $(function () {
 // add event listener X
    // get value from input and place in variable X
       // add searched city to h2 element on html page X
+         // create api fetch URL from searched city X
 var searchEl = document.getElementById("searchBtn");
 var currentCity = document.getElementById("city");
 
 searchEl.addEventListener('click', Event => {
    var searchCityVal = document.querySelector('#searchCity').value;
-   console.log(searchCityVal);
    currentCity.textContent = searchCityVal;
+   if (searchCityVal === '') {
+      alert('You must enter a city name');
+      return
+   }   
+   var weatherUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchCityVal + '&limit=1&appid=ef319315baa0199063683f91eaf8539b';
+// fetch longitude and latitude X
+   // save lon and lat to variables X
+      // create new api call with lon and lat X
+   fetch(weatherUrl)
+.then(function (response){
+   return response.json();
+})
+.then(function (data) {
+   for (var i = 0; i < data.length; i++) {
+      var latitude = (data[i].lat);
+      var longitude = (data[i].lon);
+      console.log(latitude);
+      console.log(longitude);
+      var forcastUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=ef319315baa0199063683f91eaf8539b';
+      console.log(forcastUrl);
+   }
+})
 });
     

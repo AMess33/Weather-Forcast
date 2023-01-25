518f99cb1e378922b8ca20db2330b57ad793446c
AMess33 <amess3309@gmail.com>
2023-01-24 23:48:06 -0600

    appending current weather information to UL

--- assets/script.js
+++ assets/script.js
@@ -10,11 +10,14 @@ $(function () {
       // add searched city to h2 element on html page X
          // create api fetch URL from searched city X
 var searchEl = document.getElementById("searchBtn");
+var conditions = document.getElementById("conditions");
 var currentCity = document.getElementById("city");
+var currentWeather = document.getElementById("currentWeather");
+var searchCityVal = '';
 
 searchEl.addEventListener('click', Event => {
    var searchCityVal = document.querySelector('#searchCity').value;
-   currentCity.textContent = searchCityVal;
+   currentCity.textContent = searchCityVal.toUpperCase();
    if (searchCityVal === '') {
       alert('You must enter a city name');
       return
@@ -35,8 +38,6 @@ function geoLocate(url){
    for (var i = 0; i < data.length; i++) {
       var latitude = (data[i].lat);
       var longitude = (data[i].lon);
-      console.log(latitude);
-      console.log(longitude);
       var forecastUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=ef319315baa0199063683f91eaf8539b&units=imperial&cnt=1';
       console.log(forecastUrl);
       getForecast(forecastUrl);
@@ -53,8 +54,29 @@ function getForecast(url){
 })
 .then(function (data) {
    for (var i = 0; i < data.list.length; i++) {
-      var temperature = (data.list[i].main.temp);
-      console.log(temperature);
+      var temperature = Math.floor(data.list[i].main.temp);
+      var wind = (data.list[i].wind.speed);
+      var humidity = (data.list[i].main.humidity);
    }
-})
+
+      var currentTemp = document.createElement("li");
+      var currentWind = document.createElement("li");
+      var currentHumid = document.createElement("li");
+
+      currentTemp.textContent = "Temp: " + temperature + ' Degrees';
+      currentWind.textContent = "Wind: " + wind + 'mph';
+      currentHumid.textContent = "Humidity: " + humidity + '%';
+
+      conditions.append(currentTemp, currentWind, currentHumid);
+      // var img = document.createElement('img');
+      // var listEl = document.createElement('li');
+      // img.src = 'http://openweathermap.org/img/wn/' + (data.list[i].weather[0].icon) + '@2x.png';
+      // currentWeather.textContent = temperature + ' degrees with ' + .toUpperCase();
+      // document.getElementById('city').appendChild(img);
+      // listEl.textContent = 'Temp:' + temperature;
+      // document.getElementById('conditions').appendChild(listEl);
+      // // document.getElementById('city').appendChild(listEl)
+
+   }
+)
 }
